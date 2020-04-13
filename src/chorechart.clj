b0aa9fd20bc87172edfa1854a58cb2bd64359107
Justin Good <jgoodhcg@gmail.com>
2020-04-13 12:41:13 -0400

    Graph by chores by month

--- src/chorechart.clj
+++ src/chorechart.clj
@@ -22,7 +22,7 @@
 (oz/view! line-plot)
 
 
-(defn get-chores [db-id key]
+(defn get-chores! [db-id key]
   (loop [response (-> (http/get
                        (str
                         "https://api.airtable.com/v0/"
@@ -46,7 +46,7 @@
              (json/read-str :key-fn keyword))
          (concat chores (->> response :records)))))))
 
-(def chores-raw (get-chores "app0ASuEp9abRqV2v" "keyuu0VR2QuX2RMJf"))
+(def chores-raw (get-chores! "app0ASuEp9abRqV2v" "keyuu0VR2QuX2RMJf"))
 
 (def chores-by-day-data
   (->> chores-raw
@@ -78,3 +78,36 @@
 
 (oz/view! chores-by-day)
 
+(def chores-by-month-data
+  (->> chores-raw
+       (sp/transform [sp/ALL] (fn [{:keys [id fields]}]
+                                {:id     id
+                                 :date   (:Date fields)
+                                 :chore  (:Chore fields)
+                                 :person (:Person fields)}))
+       (group-by (fn [item]
+                   (-> item (:date)
+                       (clojure.string/split #"-")
+                       ((fn [[y m d]] (str y "-" m))))))
+       (map (fn [[date chores]]
+              [{:date   date
+                :count  (->> chores
+                             (filter #(= "Justin" (:person %)))
+                             (count))
+                :person "Justin"}
+               {:date   date
+                :count  (->> chores
+                             (filter #(= "Kaiti" (:person %)))
+                             (count))
+                :person "Kaiti"}]))
+       (flatten)
+       (sort-by :date)))
+
+
+(def chores-by-month {:data   {:values chores-by-month-data}
+                      :encoding {:x     {:field "date" :type "nominal"}
+                                 :y     {:field "count" :type "quantitative"}
+                                 :color {:field "person" :type "nominal"}}
+                      :mark     "bar"})
+
+(oz/view! chores-by-month)
