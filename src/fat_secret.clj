d5cdc83e53f51a42eab7b18f7642c59a4af1bdb8
justin <jgoodhcg@gmail.com>
2020-06-28 14:01:45 -0400

    Get the initial partitioning working

--- src/fat_secret.clj
+++ src/fat_secret.clj
@@ -1,12 +1,6 @@
 (ns fat-secret
   (:require [clojure.spec.alpha :as s]))
 
-(with-open [rdr (clojure.java.io/reader "/home/justin/Desktop/Food Diary May 2019.eml")]
-  (->> rdr
-       (line-seq)
-       (drop 19)
-       (first)))
-
 (def day-heading-example "\"Saturday, June 1, 2019\",2614,112.19,56.411,199.95,24.4,35.59,114.91,2850,130,1961")
 
 (defn blank-or-float? [v]
@@ -14,7 +8,7 @@
       (some? (try (Float/parseFloat v)
                   (catch NumberFormatException e nil)))))
 
-(s/conform
+(s/def ::day-heading
   (s/cat
     :day-of-week #{"\"Monday" "\"Tuesday" "\"Wednesday" "\"Thursday" "\"Friday" "\"Saturday" "\"Sunday"}
     :month-day #(re-matches #"^\s{1}(January|Febuary|March|April|May|June|July|August|September|October|November|December){1}\s[0-9]{1,2}" %)
@@ -30,14 +24,12 @@
     :prot blank-or-float?
     :sod blank-or-float?
     :chol blank-or-float?
-    :potassium blank-or-float?
-    )
+    :potassium blank-or-float?))
 
-  (do
-    (-> (clojure.string/split day-heading-example #",")
-        ;;(->> (take 4))
-
-        ;; to throw in some new stuff at the *end* of the col
-        ;;(vec) ;; need to add _cheaply_ to the back
-        ;;(conj "0")
-        )))
+(with-open [rdr (clojure.java.io/reader "/home/justin/Desktop/Food Diary May 2019.eml")]
+  (doall
+    (->> rdr
+         (line-seq)
+         (partition-by (fn [s] (s/valid? ::day-heading (-> (clojure.string/split s #",")))))
+         (drop 1)
+         )))
