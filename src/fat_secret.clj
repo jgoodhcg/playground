2cd8c4e8dcb9741ed2c193ecd034ec8633ddb635
justin <jgoodhcg@gmail.com>
2020-06-28 19:11:06 -0400

    Parse meal headings and nest

--- src/fat_secret.clj
+++ src/fat_secret.clj
@@ -1,7 +1,10 @@
 (ns fat-secret
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [com.rpl.specter :as sp]))
 
 (def day-heading-example "\"Saturday, June 1, 2019\",2614,112.19,56.411,199.95,24.4,35.59,114.91,2850,130,1961")
+
+(def meal-heading-example " Breakfast,4,0.5,0,0,0,0,0,15,0,")
 
 (defn blank-or-float? [v]
   (or (= v "")
@@ -26,21 +29,47 @@
     :chol blank-or-float?
     :potassium blank-or-float?))
 
+(s/def ::meal-heading
+  (s/cat
+    :meal #{" Breakfast" " Lunch" " Dinner" " Snacks/Other"}
+    ;; should check on the order in the export sheet before trusting this
+    :cals blank-or-float?
+    :fat blank-or-float?
+    :sat blank-or-float?
+    :carbs blank-or-float?
+    :fiber blank-or-float?
+    :sugar blank-or-float?
+    :prot blank-or-float?
+    :sod blank-or-float?
+    :chol blank-or-float?
+    :potassium (s/? blank-or-float?)))
+
 (with-open [rdr (clojure.java.io/reader "/home/justin/Desktop/Food Diary May 2019.eml")]
   (doall
     (->> rdr
          (line-seq)
          (map #(clojure.string/split % #","))
-         (partition-by (fn [s] (s/valid? ::day-heading s)))
-         (drop 1)
          (reduce
-           (fn [data i]
-             (let [item           (first i)
-                   maybe-day-data (s/conform ::day-heading item)]
+           (fn [data item]
+             (let [maybe-day-data  (s/conform ::day-heading item)
+                   maybe-meal-data (s/conform ::meal-heading item)]
                (if (not= :clojure.spec.alpha/invalid maybe-day-data)
                  (conj data (merge maybe-day-data
-                                   {:meals []}))
-                 data
+                                   {:meals {}}))
+                 (if (not= :clojure.spec.alpha/invalid maybe-meal-data)
+                   ;; use specter to transform the last element in data to add the meals to that day
+                   (->> data
+                        (sp/transform
+                          [sp/LAST :meals]
+                          (fn [meals]
+                            (assoc meals (-> maybe-meal-data
+                                             (:meal)
+                                             (clojure.string/trim)
+                                             (clojure.string/lower-case)
+                                             (clojure.string/replace #"/" "-")
+                                             (keyword))
+                                   (merge maybe-meal-data {:items []})))))
+                   data)
                  )))
            [])
          )))
