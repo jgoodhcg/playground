0678424af25a71846caf5aacf27a84e0be6f518d
justin <jgoodhcg@gmail.com>
2020-07-08 21:08:50 -0400

    Convert to transducers

--- src/fat_secret.clj
+++ src/fat_secret.clj
@@ -101,90 +101,90 @@
     x))
 
 (defn parse-detailed-export [file]
-  (with-open [rdr (clojure.java.io/reader file)]
-    (doall
-      (->> rdr
-           (line-seq)
-           (map #(clojure.string/split % #","))
-           (reduce
-             (fn [data item]
-               (let [maybe-day-data  (->> item (s/conform ::day-heading) (zero-fill))
-                     maybe-meal-data (->> item (s/conform ::meal-heading) (zero-fill))
-                     maybe-item-data (->> item (s/conform ::item-heading) (zero-fill))]
+  (let [split-lines (with-open [rdr (clojure.java.io/reader file)]
+                      (into []
+                            (map #(clojure.string/split % #","))
+                            (line-seq rdr)))]
+    (->> split-lines
+         (reduce
+           (fn [data item]
+             (let [maybe-day-data  (->> item (s/conform ::day-heading) (zero-fill))
+                   maybe-meal-data (->> item (s/conform ::meal-heading) (zero-fill))
+                   maybe-item-data (->> item (s/conform ::item-heading) (zero-fill))]
 
-                 ;; when it is a day
-                 (if (valid-conform? maybe-day-data)
-                   ;; build out the day and leave meals empty
-                   (conj data (merge maybe-day-data
-                                     {:meals {}}))
-                   ;; otherwise check if it's a meal
-                   (if (valid-conform? maybe-meal-data)
-                     ;; when it is then build out the meal data and leave items empty
-                     (let [meal-keyword (-> maybe-meal-data
-                                            (:meal)
-                                            (clojure.string/trim)
-                                            (clojure.string/lower-case)
-                                            (clojure.string/replace #"/" "-")
-                                            (keyword))]
+               ;; when it is a day
+               (if (valid-conform? maybe-day-data)
+                 ;; build out the day and leave meals empty
+                 (conj data (merge maybe-day-data
+                                   {:meals {}}))
+                 ;; otherwise check if it's a meal
+                 (if (valid-conform? maybe-meal-data)
+                   ;; when it is then build out the meal data and leave items empty
+                   (let [meal-keyword (-> maybe-meal-data
+                                          (:meal)
+                                          (clojure.string/trim)
+                                          (clojure.string/lower-case)
+                                          (clojure.string/replace #"/" "-")
+                                          (keyword))]
 
-                       ;; some brittle state
-                       ;; saves the the meal keyword, :breakfast :lunch ... , for the next line processing
-                       ;; the next line will either be a meal item that should go into the saved meal or a new meal
-                       (reset! last-meal-edited meal-keyword)
+                     ;; some brittle state
+                     ;; saves the the meal keyword, :breakfast :lunch ... , for the next line processing
+                     ;; the next line will either be a meal item that should go into the saved meal or a new meal
+                     (reset! last-meal-edited meal-keyword)
 
-                       ;; use specter to transform the last day in data to add the meal
+                     ;; use specter to transform the last day in data to add the meal
+                     (->> data
+                          (sp/transform
+                            [sp/LAST :meals meal-keyword]
+                            (fn [_]
+                              (merge maybe-meal-data {:items []})))))
+
+                   ;; if the line has fallen through this far then
+                   ;; it's either an item or an item amount
+                   ;; we can do some loose counting that should be write most of the time
+                   (let [is-item-heading   (-> item
+                                               (count)
+                                               (> 2))
+                         is-item-amount    (-> item
+                                               (count)
+                                               (<= 2))
+                         prev-meal-keyword @last-meal-edited]
+
+                     (if (and is-item-heading
+                              (valid-conform? maybe-item-data))
+                       ;; when it is a meal-item then add it to the last day's last meal's :items list
+                       ;; there are some lines that might be misinterpreted by this loose conform
+                       ;; those are all in the beginning of the document before a valid day
+                       ;; data should be empty until a valid day is reached
+                       ;; sp/transform won't add anything until all the levels above :items are valid
                        (->> data
                             (sp/transform
-                              [sp/LAST :meals meal-keyword]
-                              (fn [_]
-                                (merge maybe-meal-data {:items []})))))
-
-                     ;; if the line has fallen through this far then
-                     ;; it's either an item or an item amount
-                     ;; we can do some loose counting that should be write most of the time
-                     (let [is-item-heading   (-> item
-                                                 (count)
-                                                 (> 2))
-                           is-item-amount    (-> item
-                                                 (count)
-                                                 (<= 2))
-                           prev-meal-keyword @last-meal-edited]
-
-                       (if (and is-item-heading
-                                (valid-conform? maybe-item-data))
-                         ;; when it is a meal-item then add it to the last day's last meal's :items list
-                         ;; there are some lines that might be misinterpreted by this loose conform
-                         ;; those are all in the beginning of the document before a valid day
-                         ;; data should be empty until a valid day is reached
-                         ;; sp/transform won't add anything until all the levels above :items are valid
+                              [sp/LAST :meals prev-meal-keyword :items]
+                              (fn [meal-items]
+                                (conj meal-items maybe-item-data))))
+                       ;; when it is an item amount then add the :amount key to the last item from the last meal edited on the last day in the list
+                       ;; there are some lines that might be misinterpreted by this loose conform
+                       ;; those are all in the beginning of the document before a valid day
+                       ;; data should be empty until a valid day is reached
+                       ;; sp/transform won't add anything until all the levels above :items LAST are valid
+                       (if is-item-amount
                          (->> data
                               (sp/transform
-                                [sp/LAST :meals prev-meal-keyword :items]
-                                (fn [meal-items]
-                                  (conj meal-items maybe-item-data))))
-                         ;; when it is an item amount then add the :amount key to the last item from the last meal edited on the last day in the list
-                         ;; there are some lines that might be misinterpreted by this loose conform
-                         ;; those are all in the beginning of the document before a valid day
-                         ;; data should be empty until a valid day is reached
-                         ;; sp/transform won't add anything until all the levels above :items LAST are valid
-                         (if is-item-amount
-                           (->> data
-                                (sp/transform
-                                  [sp/LAST :meals prev-meal-keyword :items sp/LAST]
-                                  (fn [meal-item]
-                                    ;; just use the raw item
-                                    (merge meal-item {:amount item}))))
-                           ;; if it isn't a day, meal, meal-item, or meal-item-amount then it is we shouldn't do anything with it
-                           (do
-                             (println "Following line was not a day, meal, item, or amount")
-                             (println item)
-                             (println)
-                             data))))))))
-             [])))))
+                                [sp/LAST :meals prev-meal-keyword :items sp/LAST]
+                                (fn [meal-item]
+                                  ;; just use the raw item
+                                  (merge meal-item {:amount item}))))
+                         ;; if it isn't a day, meal, meal-item, or meal-item-amount then it is we shouldn't do anything with it
+                         (do
+                           (println "Following line was not a day, meal, item, or amount")
+                           (println item)
+                           (println)
+                           data))))))))
+           []))))
 
 (def data
   (->> files
-       (map parse-detailed-export)
+       (into [] (map parse-detailed-export))
        (flatten)
        (sp/transform [sp/ALL]
                      (fn [{:keys [year month-day] :as day-item}]
@@ -220,8 +220,6 @@
                                :axis     {:labelExpr "[timeFormat(datum.value, '%b'), timeFormat(datum.value, '%m') == '01' ? timeFormat(datum.value, '%Y') : '']"}}
                        :y     {:field {:repeat "layer" } :type "quantitative" :aggregate "mean"}
                        :color {:datum {:repeat "layer" :type "nominal"}}}}})
-
-
 
 (def viz
   [:div
