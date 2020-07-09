e836919f65c9ff8339730e8cca7b4a8fc3a36092
justin <jgoodhcg@gmail.com>
2020-07-08 21:15:07 -0400

    Test transducer time
    "Elapsed time: 230696.127188 msecs"

--- src/fat_secret.clj
+++ src/fat_secret.clj
@@ -183,19 +183,20 @@
            []))))
 
 (def data
-  (->> files
-       (into [] (map parse-detailed-export))
-       (flatten)
-       (sp/transform [sp/ALL]
-                     (fn [{:keys [year month-day] :as day-item}]
-                       (let [clean-year      (-> year
-                                                 (str/trim "\"")
-                                                 (str/trim))
-                             clean-month-day (-> month-day
-                                                 (str/trim))]
-                         (merge day-item {:year      clean-year
-                                          :month-day clean-month-day
-                                          :date      (str/istr "~{clean-month-day}, ~{clean-year}")}))))))
+  (time
+    (->> files
+         (into [] (map parse-detailed-export))
+         (flatten)
+         (sp/transform [sp/ALL]
+                       (fn [{:keys [year month-day] :as day-item}]
+                         (let [clean-year      (-> year
+                                                   (str/trim "\"")
+                                                   (str/trim))
+                               clean-month-day (-> month-day
+                                                   (str/trim))]
+                           (merge day-item {:year      clean-year
+                                            :month-day clean-month-day
+                                            :date      (str/istr "~{clean-month-day}, ~{clean-year}")})))))))
 
 (def calories-line-plot
   {:data  {:values data}
