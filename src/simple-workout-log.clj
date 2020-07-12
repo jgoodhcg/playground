cd06b5fbea966bfdc42ed47b439c6844085c04e1
justin <jgoodhcg@gmail.com>
2020-07-12 16:09:08 -0400

    Get some cumulative reps showing

--- src/simple-workout-log.clj
+++ src/simple-workout-log.clj
@@ -8,12 +8,44 @@
 
 (def formatter (java.time.format.DateTimeFormatter/ofPattern "M/d/yy"))
 
-(def data (->> file
-               (sc/slurp-csv)
-               (map #(clojure.set/rename-keys
-                       %
-                       {(keyword "# of Reps") :Reps}))
-               (map #(merge % {:date-java (-> % :Date (ld/parse formatter))}))))
+(def start-date "2020-06-20")
+
+(def days (.between java.time.temporal.ChronoUnit/DAYS (ld/parse start-date)
+                    (ld/parse "2020-09-22")))
+
+(def pre-data (->> file
+                   (sc/slurp-csv)
+                   (map #(clojure.set/rename-keys % {(keyword "# of Reps") :Reps}))
+                   (map #(merge % {:date-java (-> % :Date (ld/parse formatter))}))
+                   (group-by #(-> % :date-java (.toString)))
+                   (sp/transform [sp/MAP-VALS]
+                                 (fn [entries]
+                                   {:total-reps (->> entries
+                                                     (map :Reps)
+                                                     (map #(Integer/parseInt %))
+                                                     (reduce + 0))
+                                    :date       (-> entries first :date-java (.toString))}))
+                   (sp/select [sp/MAP-VALS])
+                   (sort-by :date)
+                   (filter #(>= (ld/compare-to
+                                  (ld/parse (:date %))
+                                  (ld/parse start-date))
+                                0))))
+
+(def data (->> days
+               (range)
+               (map #(ld/plus-days (ld/parse start-date) %))
+               (map (fn [date]
+                      (let [ytd-reps          (->> pre-data
+                                                   (filter #(>= (ld/compare-to date (ld/parse (:date %)))
+                                                                0))
+                                                   (map :total-reps)
+                                                   (reduce + 0))
+                            maybe-workout-day (->> pre-data (some #(if (= (.toString date) (:date %))
+                                                                     % nil)))]
+                        (merge {} maybe-workout-day {:ytd-reps ytd-reps
+                                                     :date     (.toString date)}))))))
+
 
 (oz/start-server!)
 
@@ -21,17 +53,17 @@
   {:data  {:values data}
    :width 1500
    :layer [{:mark     "line"
-            :encoding {:x {:field    "Date"
-                           :type     "temporal"
-                           :timeUnit "yearmonthdate"}
-                       :y {:field "cumulative"
+            :encoding {:x {:field "date"
+                           :type  "temporal"
+                           :axis  {:format "%d %b"}}
+                       :y {:field "ytd-reps"
                            :type  "quantitative"}}}]})
 
 (def viz
-  [:div
-   [:h1 "2020 summer 10k reps"]
-   [:div
-    [:vega-lite reps-plot]]])
+[:div
+ [:h1 "2020 summer 10k reps"]
+ [:div
+  [:vega-lite reps-plot]]])
 
 
 (oz/view! viz)
