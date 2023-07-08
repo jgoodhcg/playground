233b62f1f849b24b645135cb689f3bba49946ab3
Justin <jgoodhcg@gmail.com>
2023-07-08 15:49:29 -0400

    Add pill people

--- src/quil_intro.clj
+++ src/quil_intro.clj
@@ -1,23 +1,35 @@
 (ns quil-intro
-  (:require [quil.core :as q]))
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(defrecord Pill [x y dx dy])
 
 (defn setup []
-  (q/frame-rate 1)                    ;; Set framerate to 1 FPS
-  (q/background 200))                 ;; Set the background colour to
-                                      ;; a nice shade of grey.
-(defn draw []
-  (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
-  (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
-  (q/fill (q/random 255))               ;; Set the fill colour to a random grey
+  (q/frame-rate 30)
+  (q/background 255)
+  (vec (repeatedly 50 #(->Pill (q/random (q/width))
+                               (q/random (q/height))
+                               (q/random -1 1)
+                               (q/random -1 1)))))
 
-  (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
-        x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
-        y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
-    (q/ellipse x y diam diam)))         ;; Draw a circle at x y with the correct diameter
+(defn update-pill [pill]
+  (->Pill (mod (+ (:x pill) (:dx pill)) (q/width))
+          (mod (+ (:y pill) (:dy pill)) (q/height))
+          (:dx pill)
+          (:dy pill)))
 
-(q/defsketch example                  ;; Define a new sketch named example
-  :title "Oh so many grey circles"    ;; Set the title of the sketch
-  :settings #(q/smooth 2)             ;; Turn on anti-aliasing
-  :setup setup                        ;; Specify the setup fn
-  :draw draw                          ;; Specify the draw fn
-  :size [1920 1080])                    ;; You struggle to beat the golden ratio
+(defn update-state [pills]
+  (map update-pill pills))
+
+(defn draw-state [pills]
+  (q/background 255)
+  (doseq [{:keys [x y]} pills]
+    (q/ellipse x y 50 100)))
+
+(q/defsketch example
+  :title "Wandering Pills"
+  :setup setup
+  :update update-state
+  :draw draw-state
+  :size [1920 1080]
+  :middleware [m/fun-mode])
