8e1ee671f5780f47ed615f1dc27a86ed28881626
Justin <jgoodhcg@gmail.com>
2023-09-24 20:07:30 -0400

    Analyze subscriptions

--- src/clerk_notebooks/transactions_2023_09_11.clj
+++ src/clerk_notebooks/transactions_2023_09_11.clj
@@ -1,11 +1,11 @@
 (ns clerk-notebooks.transactions-2023-09-11
   (:require [nextjournal.clerk :as clerk]
             [clojure.data.csv :as csv]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [potpuri.core :as pot]))
 
 (comment
-  (clerk/serve! {:watch-paths ["src/clerk_notebooks"]})
-  )
+  (clerk/serve! {:watch-paths ["src/clerk_notebooks"]}))
 
 ; # Hello, Clerk 👋
 (defn csv-data->maps [csv-data]
@@ -21,12 +21,12 @@
       csv/read-csv
       csv-data->maps
       (->> (filter #(contains?
-                #{"Groceries"
-                  "Restaurants"
-                  "Food Delivery"
-                  "Fast Food"
-                  "Farmer's Market"}
-                (get % "Category"))))
+                     #{"Groceries"
+                       "Restaurants"
+                       "Food Delivery"
+                       "Fast Food"
+                       "Farmer's Market"}
+                     (get % "Category"))))
       #_#_first
         (select-keys ["Amount" "Category" "Date"])))
 
@@ -56,12 +56,162 @@
  {:data     {:values data}
   :mark     {:type "bar" :width {:band 0.5}}
   :width    500
-  :encoding {
-             :x     {:field    "yearmonth"
+  :encoding {:x     {:field    "yearmonth"
                      :type     "temporal"
                      :axis     {:title "Year-Month", :format "%y-%m"}}
              :y     {:field "total"
                      :type  "quantitative"
                      :axis  {:title "Total Amount"}}
-             :color {:field "category"}
-             }})
+             :color {:field "category"}}})
+
+(def all-transactions
+  (-> "src/clerk_notebooks/transactions_2023_09_11.csv"
+      slurp
+      csv/read-csv
+      csv-data->maps))
+
+;; My stupid consolidation of descriptions with defs map and cond
+(comment
+  (def amazon-prime-regex #"Amazon Prime\*\w+")
+  (def audible-regex #"Audible\*\w+")
+  (def apple-regex #"Apple\.com/bill.*")
+  (def paypal-regex #"Paypal.*")
+
+  (def grouped-subs
+    (->> all-transactions
+         (filter #(= "Subscriptions" (get % "Category")))
+         (map #(get % "Description"))
+         (map (fn [d]
+                (cond
+                  (re-matches amazon-prime-regex d) :amazon-prime
+                  (re-matches audible-regex d)      :audible
+                  (re-matches apple-regex d)        :apple
+                  (re-matches paypal-regex d)       :paypal
+                  :else                             d)))
+         set)))
+
+;; Better chatgpt version using a map and some
+(def regex-keyword-pairs
+  {#"Amazon Prime\*\w+"      :amazon-prime
+   #"Prime Video.*"          :amazon-prime-video
+   #"Help\.hbomax.*"         :hbo
+   #"Help\.max.*"            :hbo
+   #"Audible\*\w+"           :audible
+   #"Roam.*"                 :roam
+   #"Paypal *roamresearch.*" :roam
+   #"Apple\.com/bill.*"      :apple
+   #"Pp*apple.*"             :apple
+   #"Github.*"               :github
+   #"Paypal *github Inc.*"   :github
+   #"Kindle.*"               :kindle})
+
+(defn match-keyword [{:keys [description] :as t}]
+  (let [consolidated-desc (or (->> regex-keyword-pairs
+                                   (some (fn [[r k]] (when (re-matches r description) k))))
+                              description)]
+    (merge t {:description consolidated-desc})))
+
+(def grouped-subs
+  (->> all-transactions
+       (filter #(= "Subscriptions" (get % "Category")))
+       (map (fn [record]
+              (let [{:strs [Amount Description]} record
+                    amount (Math/abs (Float. (clojure.string/replace Amount #"[^\d.-]" "")))]
+                {:amount amount :description Description})))
+       (map match-keyword)
+       (group-by :description)
+       (pot/map-vals #(->> % (map :amount) (reduce +)))))
+
+(def consolidated-data
+  (->> grouped-subs
+       (map (fn [[description amount]]
+              {:description description :amount amount}))
+       (into [])))
+
+(clerk/vl
+ {:data     {:values consolidated-data}
+  :mark     "bar"
+  :width    500
+  :encoding {:x     {:field "description"
+                     :type  "nominal"
+                     :axis  {:title "Description"}}
+             :y     {:field "amount"
+                     :type  "quantitative"
+                     :axis  {:title "Total Amount"}}}})
+
+(clerk/vl
+ {:data     {:values consolidated-data}
+  :mark     "arc"
+  :encoding {:theta {:field "amount"
+                     :type  "quantitative"}
+             :color {:field "description"
+                     :type  "nominal"}}
+  :view     {:stroke nil}
+  :width    300
+  :height   300})
+
+(clerk/vl
+ {:data     {:values consolidated-data}
+  :mark     "bar"
+  :encoding {:y {:field "description"
+                 :type  "nominal"
+                 :axis  {:title "Subscription Categories"}}
+             :x {:field "amount"
+                 :type  "quantitative"
+                 :axis  {:title "Total Amount"}}}})
+
+
+(def essential
+  #{:github
+   :roam
+   "Pp*google 1se Play Store"
+   "Paypal *spotifyusai"
+   "Paypal *hoverdotcom"
+   :kindle
+   "Pp*google Fitbit Llc"
+   "Digitalocean.com"
+   "apple icloud"
+   :apple
+   "1password Toronto Can"
+   "Openai San Francisco CA"
+   "Chatgpt Subscription San Francisco CA"
+   :audible
+   :amazon-prime
+   "Tillerhq* Trial Over Winthrop WA"
+   "Midjourney Inc. South San Fra CA"
+   "Bc.hey Email x x5333 IL"
+   "Paypal *github Inc"})
+
+(def essential-data
+  (filter (fn [{:keys [description]}]
+            (contains? essential description))
+          consolidated-data))
+
+(def essential-total (reduce + (map :amount essential-data)))
+(def overall-total (reduce + (map :amount consolidated-data)))
+(def comparison-data [{:group "Essential" :amount essential-total}
+                      {:group "Overall" :amount overall-total}])
+
+(clerk/vl
+ {:data     {:values comparison-data}
+  :mark     "bar"
+  :encoding {:x {:field "group"
+                 :type  "nominal"
+                 :axis  {:title "Group"}}
+             :y {:field "amount"
+                 :type  "quantitative"
+                 :axis  {:title "Total Amount"}}}})
+
+(def non-essential-total (- overall-total essential-total))
+(def proportion-data [{:group "Essential" :amount essential-total}
+                      {:group "Non-Essential" :amount non-essential-total}])
+
+(clerk/vl
+ {:data     {:values proportion-data}
+  :mark     "bar"
+  :encoding {:x {:field "group"
+                 :type  "nominal"
+                 :axis  {:title "Group"}}
+             :y {:field "amount"
+                 :type  "quantitative"
+                 :axis  {:title "Total Amount"}}}})
