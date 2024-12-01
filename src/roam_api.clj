bc403db8696de408c2a5e5a7ddb8224e2a446329
Justin Good <jgoodhcg@gmail.com>
2024-12-01 01:23:44 -0500

    Get roam api pull-many working

--- src/roam_api.clj
+++ src/roam_api.clj
@@ -9,8 +9,8 @@
   [:block/uid
    :node/title
    :block/string
-   {:block/children [:block/uid :block/string {:block/refs [:node/title :block/uid]}]}
-   {:block/refs [:node/title :block/string :block/uid]}])
+   {:block/children [:block/uid #_#_:block/string {:block/refs [:node/title :block/uid]}]}
+   {:block/refs [:node/title #_:block/string :block/uid]}])
 
 (def eid
   [:block/uid "ArtdSbqUV"])
@@ -22,21 +22,45 @@
     :pull      (str base-url "api/graph/" graph-name "/pull")
     :pull-many (str base-url "api/graph/" graph-name "/pull-many")))
 
-(comment
+(defn pull [selector eid]
   (-> (http/post (api "jgood-brain" :pull)
                  {:headers           {"Authorization" (str "Bearer " roam-api-token)}
-                   ;; important that selector and eid are strings of edn not json
+               ;; important that selector and eid are strings of edn not json
                   :body              (json/write-str {:selector (str selector)
-                                                      :eid      (str eid)})
+                                                      :eid     (str eid)})
                   :content-type      :json
                   :accept            :json
-                   ;; technically redirects were designed to only work on GET and HEAD based on RFCs for http
-                   ;; but people use them for post all the time
+               ;; technically redirects were designed to only work on GET and HEAD based on RFCs for http
+               ;; but people use them for post all the time
                   :redirect-strategy :lax})
       :body
       json/read-str
-      (get "result")
-      keys)
+      (get "result")))
+
+(defn pull-many [selector eids]
+  (-> (http/post (api "jgood-brain" :pull-many)
+                 {:headers           {"Authorization" (str "Bearer " roam-api-token)}
+               ;; important that selector and eid are strings of edn not json
+                  :body              (json/write-str {:selector (str selector)
+                                                      :eids     (str eids)})
+                  :content-type      :json
+                  :accept            :json
+               ;; technically redirects were designed to only work on GET and HEAD based on RFCs for http
+               ;; but people use them for post all the time
+                  :redirect-strategy :lax})
+      :body
+      json/read-str
+      (get "result")))
+
+(comment
+  (pull selector eid)
+
+  (-> (pull selector eid)
+      (get ":block/children")
+      (->> (map (fn [c] [:block/uid (get c ":block/uid")])))
+      vec
+      str
+      (->> (pull-many selector)))
 
 ;;
   )
