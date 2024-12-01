e673a4d969ce874d35fd3f86fd9636524f6e30a3
Justin Good <jgoodhcg@gmail.com>
2024-12-01 00:05:07 -0500

    Fix roam sdk result keys

--- src/roam_api.clj
+++ src/roam_api.clj
@@ -1,7 +1,9 @@
 (ns roam-api
   (:require [com.roamresearch.sdk.backend :as api]
             [secrets :refer [roam-api-token]]
-            [potpuri.core :as pot]))
+            [potpuri.core :as pot]
+            [clojure.string :as str]
+            [clojure.walk :refer [postwalk]]))
 
 (def selector
   [:block/uid
@@ -13,13 +15,30 @@
 (def eid [:block/uid "ArtdSbqUV"])
 
 (def result
-  (api/pull {:token roam-api-token :graph "jgood-brain"}
-            (str selector)
-            (str eid)))
+  (:result (api/pull {:token roam-api-token :graph "jgood-brain"}
+                     (str selector)
+                     (str eid))))
 
-(-> result :result
-    ;; ugly keyword hammer but trying to access with `:block/refs`, `::block/refs`, and even defining a ns for block doesn't work
-    (->> (pot/map-keys name))
-    (get "refs")
-    (->> (map (fn [ref]
-                (->> ref (pot/map-keys name))))))
+(defn fix-keys
+  [m]
+  ;; Results coming out of the sdk function have fully namespaced keys.
+  ;; The way I see them at the repl is with shorthand notation like `::block/refs`.
+  ;; When I try to access them with something like `(-> result :result ::block/refs)`
+  ;; it fails because `blocks` is not defined
+  ;; If I create that namespace it doesn't match.
+  ;; If I try `(-> result :result :block/refs)` it also fails because that doesn't match.
+  ;; The below fixes that so the keys match the selectors -- `:block/refs`
+  (postwalk (fn [k?]
+              (if (keyword? k?)
+                (keyword (->> k? namespace rest (str/join ""))
+                         (->> k? name))
+                k?)) m))
+
+(comment
+  (-> result
+      fix-keys
+      :block/refs
+      )
+
+  ;;
+  )
