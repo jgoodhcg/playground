6bd46dad6afba18e8c2fbd521b4affdadc87ff52
Justin Good <jgoodhcg@gmail.com>
2024-12-18 19:45:52 -0500

    Partition text files

--- src/openai/text-to-speech.clj
+++ src/openai/text-to-speech.clj
@@ -2,47 +2,46 @@
   (:require [secrets :as secrets]
             [clj-http.client :as client]
             [cheshire.core :as json]
-            [clojure.java.io :as io]))
-
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (def base-url "https://api.openai.com/v1")
 
-;; curl https://api.openai.com/v1/audio/speech \
-;;   -H "Authorization: Bearer $OPENAI_API_KEY" \
-;;   -H "Content-Type: application/json" \
-;;   -d '{
-;;     "model": "tts-1",
-;;     "input": "The quick brown fox jumped over the lazy dog.",
-;;     "voice": "alloy"
-;;   }' \
-;;   --output speech.mp3
+(defn- partition-text [text chunk-size]
+  (->> text
+       (partition-all chunk-size)
+       (map #(apply str %))))
 
-(defn save-mp3-from-text-file [{:keys [text-file-path
-                                       mp3-file-path
-                                       voice]
-                                :or {voice "onyx"}}]
-  (let [input-text (slurp text-file-path)
-        url        (str base-url "/audio/speech")
-        api-key    secrets/openai-api-key
-        response   (client/post url
-                                {:headers {"Authorization" (str "Bearer " api-key)
-                                           "Content-Type"  "application/json"}
-                                 :body    (json/encode {:model "tts-1"
-                                                        :voice voice
-                                                        :input input-text})
-                                 :as      :byte-array})]
-    (with-open [out (io/output-stream mp3-file-path)]
-      (.write out (:body response)))))
+(defn save-mp3-from-text-file
+  [{:keys [text-file-path mp3-file-path voice]
+    :or {voice "onyx"}}]
+  (let [input-text  (slurp text-file-path)
+        parts       (partition-text input-text 4000)
+        base-name   (if (str/ends-with? mp3-file-path ".mp3")
+                      (subs mp3-file-path 0 (- (count mp3-file-path) 4))
+                      mp3-file-path)
+        url         (str base-url "/audio/speech")
+        api-key     secrets/openai-api-key]
+    (doseq [[idx part] (map-indexed vector parts)]
+      (let [response (client/post url
+                                  {:headers {"Authorization" (str "Bearer " api-key)
+                                             "Content-Type"  "application/json"}
+                                   :body    (json/encode {:model "tts-1"
+                                                          :voice voice
+                                                          :input part})
+                                   :as      :byte-array})
+            out-file (format "%s-pt-%d.mp3" base-name (inc idx))]
+        (with-open [out (io/output-stream out-file)]
+          (.write out (:body response)))))))
 
 (comment
   (save-mp3-from-text-file
-   {:text-file-path "resources/openai-audio-test.txt"
-    :mp3-file-path  "resources/eopnai-audio-test.mp3"}
-   )
+    {:text-file-path "resources/openai-audio-test.txt"
+     :mp3-file-path  "resources/eopnai-audio-test.mp3"})
 
   (save-mp3-from-text-file
-   {:text-file-path "resources/a-day-in-the-life.txt"
-    :mp3-file-path  "resources/a-day-in-the-life.mp3"}
-   )
+    {:text-file-path "resources/a-day-in-the-life.txt"
+     :mp3-file-path  "resources/a-day-in-the-life.mp3"})
+
   ;;
   )
